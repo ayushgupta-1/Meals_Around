f327eeda599168284ead39668c10ee416b4e826c
Ayush Gupta <ayushfundaa99@gmail.com>
2022-03-17 01:29:04 +0530

    commit

---
+++ src/features/restaurants/screens/restaurants.screens.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components/native";
+import { FlatList } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { SafeArea } from "../../../components/utility/safe-area.component";
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestaurantsScreen = () => {
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <FlatList
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+          { name: 7 },
+          { name: 8 },
+          { name: 9 },
+          { name: 10 },
+          { name: 11 },
+          { name: 12 },
+          { name: 13 },
+          { name: 14 },
+        ]}
+        renderItem={() => <RestaurantInfoCard />}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ paddingHorizontal: 16 }}
+      />
+    </SafeArea>
+  );
+};
