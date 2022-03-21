87ed83de5adee02382571e494582cca8f28750c4
Ayush Gupta <ayushfundaa99@gmail.com>
2022-03-21 18:23:14 +0530

    progressive commit

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
 
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
@@ -13,13 +16,43 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBm9rrX06_S3fVCXkljJODVIhnNIqJK_ug",
+  authDomain: "mealstogo-e36ed.firebaseapp.com",
+  projectId: "mealstogo-e36ed",
+  storageBucket: "mealstogo-e36ed.appspot.com",
+  messagingSenderId: "106827352730",
+  appId: "1:106827352730:web:5503d857bf630b8ddeed23",
+};
+
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      signInWithEmailAndPassword(auth, "ayush@gupta.io", "54321q")
+        .then((user) => {
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }, 2000);
+  }, []);
+
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
   const [latoLoaded] = useLato({ Lato_400Regular });
-
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
+  if (!isAuthenticated) return null;
 
   return (
     <>
--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -13,8 +13,10 @@ const RestaurantStack = createStackNavigator();
 export const RestaurantsNavigator = () => {
   return (
     <RestaurantStack.Navigator
-      headerMode="none"
-      screenOptions={{ ...TransitionPresets.ModalPresentationIOS }}
+      screenOptions={{
+        headerShown: false,
+        ...TransitionPresets.ModalPresentationIOS,
+      }}
     >
       <RestaurantStack.Screen
         name="Restaurants"
